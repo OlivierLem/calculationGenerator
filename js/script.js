3fc03a68ae51e6580cec8755bc1dd3aebb87f829
OlivierL <lemort.olivier.pro@gmail.com>
2022-03-28 11:39:41 +0200

    système de module commenter pour utiliser le fichier hors serveur

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { retenueCalc, randNumber } from "./fonctionUtilitaire.js";
+//import { retenueCalc, randNumber } from "./fonctionUtilitaire.js";
 
 const reduct = document.querySelector('.arrow ')
 const generateCalc =document.querySelector('.generateurCalcul')
